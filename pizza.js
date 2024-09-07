ffe9ec24edcd04b9fef57173590119b434bca829
alaaelfauomiy <alaamoatzbellah@gmail.com>
2024-09-07 11:54:33 +0300

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,20 @@
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+var prodace=[
+   "0",
+   console.log()
+];
+function sum(){
+   var add= document.getElementById("s").value;
+   document.getElementById("a").innerHTML=add
+}
+function sum1(){
+   var add= document.getElementById("ss").value;
+   document.getElementById("a").innerHTML=add
+}
+function printr(){
+   window.print()
+}
